c419657ff047ffaafbde6cdc15d39e3cffd4d4be
se6PostulateLms <163225108+subashsg7777@users.noreply.github.com>
2026-01-01 19:27:27 +0530

    now v1 of SGDisposals Backend Completed

--- src/main/java/com/subash/SGDisposals/CorsConfig.java
+++ src/main/java/com/subash/SGDisposals/CorsConfig.java
@@ -10,10 +10,11 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true);
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                .allowedHeaders("*");
     }
 }
 
+
+// new  
